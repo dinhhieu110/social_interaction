6d7c07579c26b4c06ca937b36c6cc2d71ba34768
dinhhieu110 <trandinhhieu11002@gmail.com>
2025-03-01 13:17:57 +0700

    [SI-02][Client & Server] Basic folder structures

---
+++ server/src/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
+
+const app = express();
+
+app.use("/api/auth", authRoutes);
+
+const PORT = 5001;
+
+app.listen(PORT, () => {
+  console.log(`server is running on ${PORT}`);
+});
