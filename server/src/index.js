e95b18812d90b2d77a5009b3b03092181ab6e576
dinhhieu110 <trandinhhieu11002@gmail.com>
2025-03-01 13:46:04 +0700

    [SL-03][Server] Database Setup

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,17 @@
 import express from "express";
+import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
+
+dotenv.config();
 
 const app = express();
 
 app.use("/api/auth", authRoutes);
 
-const PORT = 5001;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-  console.log(`server is running on ${PORT}`);
+  console.log(`server is running on PORT: ${PORT}`);
+  connectDB();
 });
