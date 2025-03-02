15823304fdbdc6f9173c19fae7d00fd9d40ef59b
dinhhieu110 <trandinhhieu11002@gmail.com>
2025-03-02 17:59:35 +0700

    [SI-05][Server] Login & Logout

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import User from "../models/user.model.js";
-import bcrypt from "bcryptjs";
 import { generateToken } from "../lib/utils.js";
+import bcrypt from "bcryptjs";
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
@@ -16,8 +16,6 @@ export const signup = async (req, res) => {
 
     const user = await User.findOne({ email });
     if (user) return res.status(400).json({ message: "User already exists!" });
-
-    // hash password()
     const salt = await bcrypt.genSalt(10);
     const hashPassword = await bcrypt.hash(password, salt);
 
@@ -45,10 +43,44 @@ export const signup = async (req, res) => {
   }
 };
 
-export const login = (req, res) => {
-  res.send("Login route");
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ message: "All fields are required!" });
+  }
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "This account does not exist!" });
+    } else {
+      const isPasswordCorrect = await bcrypt.compare(password, user.password);
+      console.log("isPasswordCorrect: ", isPasswordCorrect);
+      if (isPasswordCorrect) {
+        generateToken(user._id, res);
+        return res.status(200).json({
+          _id: user._id,
+          fullname: user.fullName,
+          email: user.email,
+          avatar: user.avatar,
+        });
+      } else {
+        return res.status(400).json({ message: "Invalid credential!" });
+      }
+    }
+  } catch (error) {
+    console.log("Error in Login controller: ", error.message);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 };
 
 export const logout = (req, res) => {
-  res.send("Logout route");
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    return res.status(200).json({ message: "Logout successfully!" });
+  } catch (error) {
+    console.log("Error in Logout controller: ", error.message);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 };
+
+export const updateProfile = async (req, res) => {};
