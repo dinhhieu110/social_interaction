70f02eabba71b1c0c816e074f2d52ddfbfe12e6b
dinhhieu110 <trandinhhieu11002@gmail.com>
2025-03-03 15:14:52 +0700

    [SI-08][Server][Server] Handle Cors

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import cors from "cors"
 
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
@@ -16,6 +17,13 @@ app.use(express.urlencoded({ extended: true }));
 
 // Allow to pass the cookies
 app.use(cookieParser());
+app.use(cors
+  ({
+    origin: "http://localhost:5173",
+    credentials: true
+  })
+
+)
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
