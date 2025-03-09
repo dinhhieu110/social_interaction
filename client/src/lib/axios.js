da80ef8c9f8e5d6cbe28f8bcc2dc7a4de1fa4929
dinhhieu110 <trandinhhieu11002@gmail.com>
2025-03-09 22:48:13 +0700

    [SI-19] Final Stage - Deployment configuration

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,7 +1,10 @@
-import axios from "axios"
+import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5001/api",
-    // enable to send cookie every single request
-    withCredentials: true,
-})
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5001/api"
+      : "/api",
+  // enable to send cookie every single request
+  withCredentials: true,
+});
--- server/src/index.js
+++ server/src/index.js
@@ -3,12 +3,15 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import cors from "cors";
+import path from "path";
 
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 // Convert payload before sending to server
 app.use(express.json()); // This enables JSON body parsing
@@ -26,8 +29,13 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-const PORT = process.env.PORT;
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/dist")));
 
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
+  });
+}
 server.listen(PORT, () => {
   console.log(`server is running on PORT: ${PORT}`);
   connectDB();
